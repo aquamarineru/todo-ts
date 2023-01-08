0c54f4743758a6675f71075c74152af3bbb0c180
Marina Ilyushina <m_ilyushina@hotmail.com>
2023-01-08 20:13:26 +0100

    states & zustand

---
+++ src/data/stores/useTodoStore.ts
@@ -0,0 +1,50 @@
+import create from 'zustand';
+import { generateId } from '../helper';
+
+interface Task {
+    id: string;
+    title: string;
+    createdAt: number;
+}
+interface TodoStore {
+    tasks: Task[];
+    createTask: (title: string) => void;
+    updateTask: (id: string, title: string) => void;
+    removeTask: (id: string) => void;
+}
+export const useTodoStore = create<TodoStore>((set, get) => ({
+    tasks: [
+        {
+            id: 'iloveapple',
+            title: 'apple',
+            createdAt: Date.now()
+        }
+    ],
+    createTask: (title) => {
+        const { tasks } = get();
+        const newTask = {
+            id: generateId(),
+            title,
+            createdAt: Date.now()
+        }
+        set({ 
+            // tasks: [...tasks, newTask] 
+            tasks: [newTask].concat(tasks)
+        });
+    },
+    updateTask: (id: string, title: string) => {
+        const { tasks } = get();
+        set({ 
+            tasks: tasks.map((task) =>({
+                ...task,
+                title: task.id === id ? title : task.title
+            }))
+        });
+    },
+    removeTask: (id: string) => {
+        const { tasks } = get();
+        set({ 
+            tasks: tasks.filter((task) =>(task.id !== id))
+        });
+    }
+}));
