d100a3d384e76cde1aaebc67ebbddbafa9746b2e
Marina Ilyushina <m_ilyushina@hotmail.com>
2023-01-08 19:06:55 +0100

    scss main styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './view/app/index'
 
+import './view/styles/reset.scss'
+import './view/styles/common.scss'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
